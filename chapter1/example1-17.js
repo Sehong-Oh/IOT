db79527c77d0e177f0acc375898ac845d399fb87
Oh Sehong <ohsehong@Ohui-MacBookAir.local>
2025-05-02 22:54:26 +0900

    예제1-17 : 깊은 복사 결과 확인

---
+++ chapter1/example1-17.js
@@ -0,0 +1,17 @@
+// 예제 1-17
+var obj = {
+    a: 1,
+    b: {
+        c: null,
+        d: [1, 2]
+    }
+};
+
+var obj2 = copyObjectDeep(obj);
+
+obj2.a = 3;
+obj2.b.c = 4;
+obj2.b.d[1] = 3;
+
+console.log(obj)
+console.log(obj2)
